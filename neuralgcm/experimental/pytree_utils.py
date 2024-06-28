46c7c03c5f1d4ea2777def5a8d6c44a7705e4c3a
Dmitrii Kochkov <dkochkov@google.com>
2024-06-27 19:55:07 -0700

    No public description
    PiperOrigin-RevId: 647524375

---
+++ neuralgcm/experimental/pytree_utils.py
@@ -0,0 +1,365 @@
+# Copyright 2024 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Utility functions that operate on pytrees."""
+
+from collections import abc
+import dataclasses
+import functools
+from typing import Any, Callable, Sequence
+
+import jax
+import jax.numpy as jnp
+from neuralgcm.experimental import typing
+import numpy as np
+
+
+def pack_pytree(pytree: typing.Pytree, axis: int = -3) -> typing.Array:
+  """Packs `pytree` by concatenating leaves along `axis`."""
+  flat, _ = jax.tree.flatten(pytree)
+  if not flat:
+    return None  # pytype: disable=bad-return-type  # jax-ndarray
+  packed = jnp.concatenate(flat, axis)
+  return packed
+
+
+def unpack_to_pytree(
+    array: typing.Array, pytree_of_shapes: typing.Pytree, axis: int = -3
+) -> typing.Pytree:
+  """Unpacks an `array` into a pytree with shapes `pytree_of_shapes`."""
+  shapes, tree_def = jax.tree.flatten(pytree_of_shapes)
+  splits = np.cumsum(np.array([x[axis] for x in shapes]))[:-1]
+  split = jnp.split(array, splits, axis)
+  return jax.tree.unflatten(tree_def, split)
+
+
+def stack_pytree(pytree: typing.Pytree, axis: int = 0) -> typing.Array:
+  """Stacks `pytree` by stacking leaves along a new axis."""
+  flat, _ = jax.tree.flatten(pytree)
+  if not flat:
+    return None  # pytype: disable=bad-return-type  # jax-ndarray
+  stacked = jnp.stack(flat, axis)
+  return stacked
+
+
+def unstack_to_pytree(
+    array: typing.Array, pytree_of_shapes: typing.Pytree, axis: int = 0
+) -> typing.Pytree:
+  """Unstacks an `array` into a pytree with shapes `pytree_of_shapes`."""
+  _, tree_def = jax.tree.flatten(pytree_of_shapes)
+  # `array` is split along `axis` and resulting singleton dimension is removed
+  split = jnp.split(array, array.shape[axis], axis)
+  split = jax.tree.map(lambda x: jnp.squeeze(x, axis=axis), split)
+  return jax.tree.unflatten(tree_def, split)
+
+
+def tree_map_where(
+    condition_fn: Callable[[typing.Array], typing.Array],
+    f: Callable[[typing.Array], typing.Array],
+    g: Callable[[typing.Array], typing.Array],
+    x: typing.Pytree,
+) -> typing.Pytree:
+  """Map `f` over pytree leaves if condition_fn(leaf), else use `g`."""
+
+  def tm_where(x: typing.Array) -> typing.Array:
+    return f(x) if condition_fn(x) else g(x)
+
+  return jax.tree.map(tm_where, x)
+
+
+def tree_map_over_nonscalars(
+    f: Callable[[typing.Array], typing.Array],
+    x: typing.Pytree,
+    *,
+    scalar_fn: Callable[[typing.Array], typing.Array] = lambda x: x,
+    backend: str = 'jax',
+) -> typing.Pytree:
+  """Map `f` over nonscalar pytree leaves, but use `scalar_fn` on scalars."""
+  as_array_fn = {'jax': jnp.asarray, 'numpy': np.asarray}[backend]
+
+  def g(x: typing.Array) -> typing.Array:
+    x = as_array_fn(x)
+    return f(x) if x.ndim else scalar_fn(x)
+
+  return jax.tree.map(g, x)
+
+
+def shape_structure(inputs):
+  """Returns `inputs` with leaves replaced by arrays of corresponding shapes."""
+  return jax.tree.map(lambda x: np.asarray(np.shape(x)), inputs)
+
+
+def _normalize_axis(axis: int, ndim: int) -> int:
+  """Validates and returns positive `axis` value."""
+  if not -ndim <= axis < ndim:
+    raise ValueError(f'invalid axis {axis} for ndim {ndim}')
+  if axis < 0:
+    axis += ndim
+  return axis
+
+
+def slice_along_axis(
+    inputs: typing.Pytree,
+    axis: int,
+    idx: int | slice,
+    expect_same_dims: bool = False,
+) -> typing.Pytree:
+  """Returns slice of `inputs` defined by `idx` along axis `axis`.
+
+  Args:
+    inputs: pytree to slice.
+    axis: axis along which to slice `inputs`.
+    idx: index or slice along axis `axis` that is returned.
+    expect_same_dims: whether all arrays should have same number of dimensions.
+
+  Returns:
+    Slice of `inputs` defined by `idx` along axis `axis`. If idx is an int,
+    the axis is dropped. If idx is a slice, the axis is handled accordingly.
+  """
+  arrays, tree_def = jax.tree.flatten(inputs)
+  ndims = set(a.ndim for a in arrays)
+  if expect_same_dims and len(ndims) != 1:
+    raise ValueError(
+        'arrays in `inputs` expected to have same ndims, but have '
+        f'{ndims}. To allow this, pass expect_same_dims=False'
+    )
+  elif axis < 0:
+    raise ValueError(f'Using {axis=} is error-prone if expect_same_dims=False')
+  sliced = []
+  for array in arrays:
+    ndim = array.ndim
+    slc = tuple(
+        idx if j == _normalize_axis(axis, ndim) else slice(None)
+        for j in range(ndim)
+    )
+    sliced.append(array[slc])
+  return jax.tree.unflatten(tree_def, sliced)
+
+
+def split_along_axis(
+    inputs: typing.Pytree,
+    split_idx: int,
+    axis: int,
+    expect_same_dims: bool = False,
+) -> tuple[typing.Pytree, typing.Pytree]:
+  """Returns 2 pytrees formed by splitting `inputs` along `axis` at `split_idx`.
+
+  Args:
+    inputs: pytree to split.
+    split_idx: index along `axis` where the second split starts.
+    axis: axis along which to split the `inputs`.
+    expect_same_dims: whether all arrays should have same number of dimensions.
+
+  Returns:
+    Tuple of slices of `inputs` split along `axis` at `split_idx`.
+  """
+
+  first_slice = slice_along_axis(
+      inputs, axis, slice(0, split_idx), expect_same_dims
+  )
+  second_slice = slice_along_axis(
+      inputs, axis, slice(split_idx, None), expect_same_dims
+  )
+  return first_slice, second_slice
+
+
+def split_axis(
+    inputs: typing.Pytree,
+    axis: int,
+    keep_dims: bool = False,
+) -> tuple[typing.Pytree, ...]:
+  """Splits `inputs` along `axis`.
+
+  Args:
+    inputs: pytree to be split.
+    axis: axis along which to split the `inputs`.
+    keep_dims: whether to keep `axis` dimension.
+
+  Returns:
+    Tuple of pytrees that correspond to slices of `inputs` along `axis`. The
+    `axis` dimension is removed if `squeeze is set to True.
+
+  Raises:
+    ValueError: if arrays in `inputs` don't have unique size along `axis`.
+  """
+  arrays, tree_def = jax.tree.flatten(inputs)
+  axis_shapes = set(a.shape[axis] for a in arrays)
+  if len(axis_shapes) != 1:
+    raise ValueError(f'Arrays must have equal sized axis but got {axis_shapes}')
+  (axis_shape,) = axis_shapes
+  splits = [jnp.split(a, axis_shape, axis=axis) for a in arrays]
+  if not keep_dims:
+    splits = jax.tree.map(lambda a: jnp.squeeze(a, axis), splits)
+  splits = zip(*splits)
+  return tuple(jax.tree.unflatten(tree_def, leaves) for leaves in splits)
+
+
+def concat_along_axis(
+    pytrees: Sequence[typing.Pytree], axis: int
+) -> typing.Pytree:
+  """Concatenates `pytrees` along `axis`."""
+  concat_leaves_fn = lambda *args: jnp.concatenate(args, axis)
+  return jax.tree.map(concat_leaves_fn, *pytrees)
+
+
+def as_dict(inputs: typing.Pytree) -> typing.Pytree:
+  """Returns a dict representation of `inputs` and a from_dict_fn."""
+  return_type = type(inputs)
+  if dataclasses.is_dataclass(inputs):
+    inputs = inputs.asdict()
+  else:
+    if return_type != dict:
+      raise ValueError(f'Inputs of type {return_type} are not supported.')
+
+  from_dict_fn = lambda dict_inputs: return_type(**dict_inputs)
+  return inputs, from_dict_fn
+
+
+def none_to_zeros(
+    tree: typing.Pytree,
+    reference_tree: typing.Pytree,
+) -> typing.Pytree:
+  """Returns tree where `None` is replaced with zeros_like of reference_tree."""
+  return jax.tree.map(
+      lambda x, y: jnp.zeros_like(y) if x is None else x, tree, reference_tree
+  )
+
+
+@dataclasses.dataclass(frozen=True)
+class _HashableNDArrayWrapper:
+  shape: tuple[int, ...]
+  dtype: np.dtype
+  data: bytes
+
+
+def _hash_leaf(x: typing.Pytree) -> abc.Hashable:
+  if isinstance(x, (jax.Array, np.ndarray)):
+    return _HashableNDArrayWrapper(x.shape, x.dtype, x.tobytes())
+  else:
+    return x
+
+
+def tree_hashable(x: typing.Pytree) -> abc.Hashable:
+  """Convert a pytree into something hashable."""
+  values, treedef = jax.tree.flatten(x)
+  values = tuple(map(_hash_leaf, values))
+  return values, treedef
+
+
+def tree_cache(func):
+  """Like functools.cache, but hashes with tree_hashable.
+
+  Example usage::
+
+    import numpy as np
+
+    @tree_cache
+    def f(x):
+      print('caching')
+      return x
+
+    >>> f({'a': 1, 'b': np.arange(3)})
+    caching
+    {'a': 1, 'b': array([0, 1, 2])}
+    >>> f({'a': 1, 'b': np.arange(3)})
+    {'a': 1, 'b': array([0, 1, 2])}
+
+  Args:
+    func: function to cache.
+
+  Returns:
+    Function where cached results are reused.
+  """
+  results = {}
+
+  @functools.wraps(func)
+  def wrapper(*args, **kwargs):
+    key = tree_hashable((args, kwargs))
+    if key in results:
+      return results[key]
+    result = results[key] = func(*args, **kwargs)
+    return result
+
+  return wrapper
+
+
+def flatten_dict(
+    input_dict: dict[str, Any],
+    prefix: str = '',
+    sep: str = '&',
+) -> tuple[dict[str, Any], tuple[str, ...]]:
+  """Flattens potentially nested `input_dict`."""
+  items = []
+  empty_keys = []
+  for k, v in input_dict.items():
+    if sep in k:
+      raise ValueError(f'Key {k} contains {sep=}. Use different name or sep.')
+    new_key = prefix + sep + k if prefix else k
+    if isinstance(v, dict) and v:
+      sub_dict, sub_empty_keys = flatten_dict(v, new_key, sep=sep)
+      items.extend(sub_dict.items())
+      empty_keys.extend(sub_empty_keys)
+    elif isinstance(v, dict) and not v:
+      empty_keys.append(new_key)
+    else:
+      items.append((new_key, v))
+  unique_keys, counts = np.unique(
+      np.array([x[0] for x in items]), return_counts=True
+  )
+  if (counts > 1).any():
+    raise ValueError(f'got duplicate keys {unique_keys[counts > 1]}')
+  unique_empty_keys, counts = np.unique(
+      np.array([x[0] for x in empty_keys]), return_counts=True
+  )
+  if (counts > 1).any():
+    raise ValueError(f'got duplicate keys {unique_empty_keys[counts > 1]}')
+  return dict(items), tuple(empty_keys)
+
+
+def unflatten_dict(
+    flat_dict: dict[str, Any],
+    empty_keys: tuple[str, ...] = tuple(),
+    sep: str = '&',
+) -> dict[str, Any]:
+  """Unflattens `flat_dict` with structure specified with separataion `sep`."""
+  result = dict()
+  empty_key_dict = {k: {} for k in empty_keys}
+  for key, value in (flat_dict | empty_key_dict).items():
+    sub_keys = key.split(sep)
+    sub_dict = result
+    for sub_key in sub_keys[:-1]:
+      if sub_key in sub_dict:
+        sub_dict = sub_dict[sub_key]
+      else:
+        sub_dict[sub_key] = dict()
+        sub_dict = sub_dict[sub_key]
+    sub_dict[sub_keys[-1]] = value
+  return result
+
+
+def replace_with_matching_or_default(
+    x: dict[str, Any],
+    replace: dict[str, Any],
+    default: Any = None,
+    check_used_all_replace_keys: bool = True,
+) -> dict[str, Any]:
+  """Returns `x` structure with leaves from `replace` or `default`."""
+  flat_x, empty_keys = flatten_dict(x)
+  flat_replace, _ = flatten_dict(replace)
+  if check_used_all_replace_keys:
+    unused_replace_keys = set(flat_replace.keys()) - set(flat_x.keys())
+    if unused_replace_keys:
+      raise ValueError(f'Keys {unused_replace_keys} not present in {x.keys()=}')
+  flat_result = {k: flat_replace.get(k, default) for k in flat_x.keys()}
+  return unflatten_dict(flat_result, empty_keys)
---
+++ neuralgcm/experimental/pytree_utils_test.py
@@ -0,0 +1,356 @@
+# Copyright 2024 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for pytee_utils."""
+from absl.testing import absltest
+from absl.testing import parameterized
+import chex
+import jax
+import jax.numpy as jnp
+from neuralgcm.experimental import pytree_utils
+from neuralgcm.experimental import typing
+import numpy as np
+import tree_math
+
+
+@tree_math.struct
+class TestTreeMathStruct:
+  array_attr: typing.Array
+  float_attr: float
+  dict_attr: dict[str, float]
+
+
+class PytreeUtilsTest(parameterized.TestCase):
+
+  @parameterized.parameters(
+      dict(
+          pytree={
+              'a': np.arange(8).reshape((2, 2, 2)),
+              'b': np.ones((2, 2, 2)),
+          },
+          axis=0,
+      ),
+      dict(
+          pytree=(np.arange(16).reshape((2, 4, 2)), np.zeros((2, 2, 2))),
+          axis=1,
+      ),
+      dict(
+          pytree=(np.arange(4).reshape((4, 1)), np.zeros((4, 5))),
+          axis=-1,
+      ),
+  )
+  def test_tree_pack_unpack_roundtrip(self, pytree, axis):
+    """Tests that packing -> unpacking a pytree preserves the data."""
+    pytree_shapes = pytree_utils.shape_structure(pytree)
+    packed = pytree_utils.pack_pytree(pytree, axis=axis)
+    unpacked = pytree_utils.unpack_to_pytree(packed, pytree_shapes, axis=axis)
+    chex.assert_trees_all_close(pytree, jax.device_get(unpacked))
+
+  @parameterized.parameters(
+      dict(
+          pytree={
+              'a': np.array([1, 2, 3]),
+              'b': np.array([4, 5, 6]),
+              'c': {'d': np.array([7, 8, 9])},
+          },
+          axis=0,
+          stacked_expected=np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+      ),
+      dict(
+          pytree={
+              'a': np.array([1, 2, 3]),
+              'b': np.array([4, 5, 6]),
+              'c': {'d': np.array([7, 8, 9])},
+          },
+          axis=1,
+          stacked_expected=np.array([[1, 4, 7], [2, 5, 8], [3, 6, 9]]),
+      ),
+      dict(
+          pytree={
+              'a': np.ones((1, 2, 4)),
+              'b': np.ones((1, 2, 4)),
+              'c': {'d': np.ones((1, 2, 4))},
+          },
+          axis=-2,
+          stacked_expected=np.ones((1, 2, 3, 4)),
+      ),
+  )
+  def test_tree_stack_and_unstack(self, pytree, stacked_expected, axis):
+    """Tests that tree stacking/roundtrip returns expected values."""
+    stacked = pytree_utils.stack_pytree(pytree, axis)
+    with self.subTest('stack_pytree'):
+      chex.assert_trees_all_close(jax.device_get(stacked), stacked_expected)
+    with self.subTest('unstack_pytree'):
+      unstack_shapes = pytree_utils.shape_structure(pytree)
+      unstacked = pytree_utils.unstack_to_pytree(stacked, unstack_shapes, axis)
+      chex.assert_trees_all_close(pytree, jax.device_get(unstacked))
+    with self.subTest('jit'):
+
+      def roundtrip(x):
+        stacked = pytree_utils.stack_pytree(x, axis)
+        unstack_shapes = pytree_utils.shape_structure(x)
+        return pytree_utils.unstack_to_pytree(stacked, unstack_shapes, axis)
+
+      roundtrip_fn = jax.jit(roundtrip)
+      chex.assert_trees_all_close(pytree, jax.device_get(roundtrip_fn(pytree)))
+
+  @parameterized.parameters(
+      dict(pytree=(np.zeros((6, 3)), np.ones((6, 2, 2))), idx=3, axis=0),
+      dict(pytree=(np.zeros((3, 8)), np.ones((6, 8))), idx=3, axis=1),
+      dict(pytree={'a': np.zeros((3, 9)), 'b': np.ones((6, 9))}, idx=3, axis=1),
+      dict(pytree=np.zeros((13, 5)), idx=6, axis=0),
+      dict(pytree=(np.zeros(9), (np.ones((9, 1)), np.ones(9))), idx=6, axis=0),
+  )
+  def test_split_and_concat(self, pytree, idx, axis):
+    """Tests that split_along_axis, concat_along_axis return expected shapes."""
+    split_a, split_b = pytree_utils.split_along_axis(pytree, idx, axis, False)
+    with self.subTest('split_shape'):
+      self.assertEqual(jax.tree.leaves(split_a)[0].shape[axis], idx)
+
+    reconstruction = pytree_utils.concat_along_axis([split_a, split_b], axis)
+    with self.subTest('split_concat_roundtrip'):
+      chex.assert_trees_all_close(reconstruction, pytree)
+
+    same_ndims = len(set(a.ndim for a in jax.tree.leaves(reconstruction))) == 1
+    if not same_ndims:
+      with self.subTest('raises_when_wrong_ndims'):
+        with self.assertRaisesRegex(ValueError, 'arrays in `inputs` expected'):
+          _, _ = pytree_utils.split_along_axis(pytree, idx, axis, True)
+
+    with self.subTest('multiple_concat_shape'):
+      arrays = [split_a, split_a, split_b, split_b]
+      double_concat = pytree_utils.concat_along_axis(arrays, axis)
+      actual_shape = jax.tree.leaves(double_concat)[0].shape[axis]
+      expected_shape = jax.tree.leaves(pytree)[0].shape[axis] * 2
+      self.assertEqual(actual_shape, expected_shape)
+
+  def test_pytree_cache(self):
+    """Tests that tree_cache works as expected."""
+    eval_count = 0
+
+    @pytree_utils.tree_cache
+    def cached_func(unused_arg):
+      nonlocal eval_count
+      eval_count += 1
+      return eval_count
+
+    args = {'a': 1, 'b': np.arange(3)}
+    result = cached_func(args)
+    self.assertEqual(result, 1)  # function called once.
+
+    result = cached_func(args)
+    self.assertEqual(result, 1)  # still returns 1, since called with same args.
+
+    result = cached_func('something else')
+    self.assertEqual(result, 2)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name='nested',
+          example={'a': np.arange(8), 'b': {'c': np.ones((2, 2))}, 'd': 3.14},
+      ),
+      dict(
+          testcase_name='double_nesting',
+          example={'a': {'b': {'c': 1}}, 'd': 2},
+      ),
+      dict(
+          testcase_name='nesting_same_subelements',
+          example={'a': {'b': 4.12}, 'b': 3.14},
+      ),
+      dict(
+          testcase_name='contains_empty_subdict',
+          example={'a': {'b': 4.12}, 'd': {}},
+      ),
+      dict(
+          testcase_name='contains_nested_empty_subdict',
+          example={'a': {'b': 4.12}, 'd': {'x': {}}},
+      ),
+      dict(
+          testcase_name='raises_on_sep_in_name',
+          example={'a': {'b&c': 4.12}, 'b': 3.14},
+          should_raise=True,
+      ),
+  )
+  def test_dict_flatten_unflatten_roundtrip(self, example, should_raise=False):
+    """Tests that flatten_dcit -> unflatten_dict acts as identity."""
+    if should_raise:
+      with self.assertRaises(ValueError):
+        pytree_utils.unflatten_dict(*pytree_utils.flatten_dict(example))
+    else:
+      actual = pytree_utils.unflatten_dict(*pytree_utils.flatten_dict(example))
+      chex.assert_trees_all_close(actual, example)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name='replace_root_and_nested_value',
+          inputs={'a': np.arange(8), 'b': {'c': np.ones((2, 2))}, 'd': 3.14},
+          replace_dict={'a': 2.73, 'b': {'c': np.zeros((2, 2))}},
+          default=np.ones(1),
+          expected={'a': 2.73, 'b': {'c': np.zeros((2, 2))}, 'd': np.ones(1)},
+      ),
+      dict(
+          testcase_name='double_nesting',
+          inputs={'a': {'b': {'c': 1}}, 'd': 2},
+          replace_dict={'a': {'b': {'c': 2.73}}},
+          default=1.0,
+          expected={'a': {'b': {'c': 2.73}}, 'd': 1.0},
+      ),
+      dict(
+          testcase_name='empty_subdict',
+          inputs={'a': 1, 'b': {}, 'c': 3},
+          replace_dict={'a': 2, 'c': 5},
+          default=1.0,
+          expected={'a': 2, 'b': {}, 'c': 5},
+      ),
+      dict(
+          testcase_name='nested_empty_subdict',
+          inputs={'a': 1, 'b': {}, 'c': 3, 'd': {'x': {}}},
+          replace_dict={'a': 2, 'c': 5},
+          default=1.0,
+          expected={'a': 2, 'b': {}, 'c': 5, 'd': {'x': {}}},
+      ),
+      dict(
+          testcase_name='raises_on_unused_replace_values',
+          inputs={'a': 1.2, 'b': 2.5},
+          replace_dict={'a': 2.1, 'spc_humidity': -9000},
+          default=1.0,
+          expected={'a': {'b': {'c': 2.73}}, 'd': 1.0},
+          should_raise=True,
+      ),
+  )
+  def test_replace_with_matching_or_default(
+      self,
+      inputs,
+      replace_dict,
+      default,
+      expected,
+      should_raise=False,
+  ):
+    """Tests that replace_with_matching_or_default works as expected."""
+    if should_raise:
+      with self.assertRaises(ValueError):
+        pytree_utils.replace_with_matching_or_default(
+            inputs, replace_dict, default
+        )
+    else:
+      actual = pytree_utils.replace_with_matching_or_default(
+          inputs, replace_dict, default
+      )
+      chex.assert_trees_all_close(actual, expected)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name='sqrt_1d_square_rest',
+          condition_fn=lambda x: jnp.asarray(x).ndim == 1,
+          f=lambda x: x**0.5,
+          g=lambda x: x**2,
+          pytree={'a': np.arange(8), 'b': 5, 'c': 2 * np.eye(2)},
+          expected={'a': np.arange(8) ** 0.5, 'b': 5**2, 'c': 2**2 * np.eye(2)},
+      ),
+  )
+  def test_tree_map_where(self, condition_fn, f, g, pytree, expected):
+    """Tests that tree_map_where works as expected."""
+    with self.subTest('jax_backed'):
+      actual = pytree_utils.tree_map_where(condition_fn, f, g, pytree)
+      chex.assert_trees_all_close(actual, expected)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name='sqrt_1d_square_rest',
+          f=lambda x: x**0.5,
+          scalar_fn=lambda x: x**2,
+          pytree={'a': np.arange(8), 'b': 5, 'c': 2 * np.eye(2)},
+          expected={
+              'a': np.arange(8) ** 0.5,
+              'b': 5**2,
+              'c': 2**0.5 * np.eye(2),
+          },
+      ),
+      dict(
+          testcase_name='resclae_non_time',
+          f=lambda x: x / 2,
+          scalar_fn=lambda x: x,
+          pytree={'a': np.ones(8), 'b': np.eye(2), 'time': 3.14},
+          expected={'a': np.ones(8) / 2, 'b': np.eye(2) / 2, 'time': 3.14},
+      ),
+  )
+  def test_tree_map_over_nonscalars(self, f, scalar_fn, pytree, expected):
+    """Tests that tree_map_over_nonscalars works as expected."""
+    assert_is_jax_array = lambda x: self.assertIsInstance(x, jax.Array)
+    assert_not_jax_array = lambda x: self.assertNotIsInstance(x, jax.Array)
+    with self.subTest('jax_backed'):
+      actual = pytree_utils.tree_map_over_nonscalars(
+          f=f, x=pytree, scalar_fn=scalar_fn
+      )  # jax is the default backend.
+      jax.tree_util.tree_map(assert_is_jax_array, actual)
+      chex.assert_trees_all_close(actual, expected)
+    with self.subTest('numpy_backed'):
+      actual = pytree_utils.tree_map_over_nonscalars(
+          f=f, x=pytree, scalar_fn=scalar_fn, backend='numpy'
+      )
+      jax.tree_util.tree_map(assert_not_jax_array, actual)
+      chex.assert_trees_all_close(actual, expected)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name='split_two_along_axis_0',
+          pytree={'a': np.stack([np.ones(4), np.zeros(4)]), 'b': np.arange(2)},
+          axis=0,
+          expected=(
+              {'a': np.ones(4), 'b': np.asarray(0)},
+              {'a': np.zeros(4), 'b': np.asarray(1)},
+          ),
+      ),
+      dict(
+          testcase_name='split_three_along_axis_1',
+          pytree={'a': np.eye(3), 'b': np.arange(3)[np.newaxis, :]},
+          axis=1,
+          expected=(
+              {'a': np.eye(3)[:, 0], 'b': np.asarray([0])},
+              {'a': np.eye(3)[:, 1], 'b': np.asarray([1])},
+              {'a': np.eye(3)[:, 2], 'b': np.asarray([2])},
+          ),
+      ),
+  )
+  def test_split_axis(self, pytree, axis, expected):
+    """Tests that split_axis works as expected."""
+    actual = pytree_utils.split_axis(pytree, axis=axis)
+    chex.assert_trees_all_close(actual, expected)
+    chex.assert_trees_all_equal_shapes(actual, expected)
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name='dict_input',
+          inputs={'a': np.ones((2, 2, 2)), 'b': np.arange(2), 'c': 2},
+      ),
+      dict(
+          testcase_name='nested_dict_input',
+          inputs={'a': np.ones((2, 2, 2)), 'b': np.arange(2), 'c': {'d': 4}},
+      ),
+      dict(
+          testcase_name='tree_math_struct_input',
+          inputs=TestTreeMathStruct(np.ones(10), 1.54, {'a': 0.5, 'b': 0.25}),
+      ),
+  )
+  def test_asdict_forward_and_roundtrip(self, inputs):
+    dict_repr, from_dict_fn = pytree_utils.as_dict(inputs)
+    with self.subTest('forward'):
+      self.assertIsInstance(dict_repr, dict)
+    with self.subTest('round_trip'):
+      reconstructed = from_dict_fn(dict_repr)
+      chex.assert_trees_all_close(reconstructed, inputs)
+
+
+if __name__ == '__main__':
+  absltest.main()
