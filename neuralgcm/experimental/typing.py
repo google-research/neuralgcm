50755bbdc61d2f247dc79a89da3f963ae8408bb8
Dmitrii Kochkov <dkochkov@google.com>
2024-11-27 17:39:55 -0800

    Added ObsData and ObsSpec definitions to typing.py
    PiperOrigin-RevId: 700848796

--- neuralgcm/experimental/typing.py
+++ neuralgcm/experimental/typing.py
@@ -15,31 +15,77 @@
 """Types used by neuralgcm.experimental API."""
 from __future__ import annotations
 
+import abc
 import dataclasses
+import datetime
+import functools
 from typing import Any, Callable, Generic, TypeVar
 
 import jax
+import jax.numpy as jnp
 from neuralgcm.experimental import scales
 import numpy as np
+import pandas as pd
 import tree_math
 
 
+units = scales.units
 #
 # Generic types.
 #
-Dtype = jax.typing.DTypeLike | Any
 Array = np.ndarray | jax.Array
+Dtype = jax.typing.DTypeLike | Any
 Numeric = float | int | Array
-Timestep = np.timedelta64 | float
 PRNGKeyArray = jax.Array
-units = scales.units
+ShapeDtypeStruct = jax.ShapeDtypeStruct
+ShapeFloatStruct = functools.partial(ShapeDtypeStruct, dtype=jnp.float32)
+Timestep = np.timedelta64 | float
+TimedeltaLike = str | np.timedelta64 | pd.Timestamp | datetime.timedelta
 Quantity = scales.Quantity
 
 #
 # Generic API input/output types.
 #
-PyTreeState = TypeVar('PyTreeState')
 Pytree = Any
+PyTreeState = TypeVar('PyTreeState')
+
+
+#
+# Simulation function signatures.
+#
+StepFn = Callable[[PyTreeState], PyTreeState]
+PostProcessFn = Callable[..., Pytree]
+
+
+class ObservationSpecs(abc.ABC):
+  """Base class for observation data specifications.
+
+  Observation specification objects are used to express queries to the model's
+  `observe` method. All ObservationSpecs objects should have a corresponding
+  ObservationData class that can be used to store the data, both of which must
+  be a valid jax pytree.
+
+  In many cases the difference between ObservationSpecs and ObservationData is
+  that ObservationSpecs contain supporting values and a set of
+  coordax.Coordinate objects while ObservationData is structured similarly but
+  uses coordax.Field to store data. This can be reflected by using the same
+  object that inherits from both ObservationSpecs and ObservationData and
+  supports coordax.Field and coordax.Coordinate for relevant attributes.
+  """
+
+
+class ObservationData(abc.ABC):
+  """Base class for observation data."""
+
+  @property
+  @abc.abstractmethod
+  def get_specs(self) -> ObservationSpecs:
+    """Returns ObservationSpecs that describe data specifications."""
+    ...
+
+
+Query = dict[str, ObservationSpecs]
+Observation = dict[str, ObservationData]
 
 
 @tree_math.struct
@@ -172,7 +218,6 @@ class Timestamp:
 
     >>> Timestamp.from_datetime64(np.datetime64('1970-01-02'))
     Timestamp(delta=Timedelta(days=1, seconds=0))
-
   """
 
   delta: Timedelta
